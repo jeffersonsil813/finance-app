a10247f6c29f889d965850b86e6b52587c8dea67
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-08-19 11:48:04 -0300

    feat: wrap logo with link to home page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
 import Logo from "@/components/logo";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col w-full max-w-100 m-auto space-y-2">
-      <Logo />
+      <Link href="/" className="cursor-default">
+        <Logo />
+      </Link>
       {children}
     </div>
   );
