1360e78fe52641a522f46f0999d8fbf5ef8f886b
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-08-19 11:20:01 -0300

    feat: create app logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,14 @@
+import { TrendingUp } from "lucide-react";
+
+const Logo = () => {
+  return (
+    <div className="flex flex-nowrap gap-2 items-center justify-center">
+      <div className="bg-light-green rounded-full w-8 h-8 flex items-center justify-center">
+        <TrendingUp className="text-white" width={18} height={18} />
+      </div>
+      <h1 className="font-semibold text-xl">Finance</h1>
+    </div>
+  );
+};
+
+export default Logo;
