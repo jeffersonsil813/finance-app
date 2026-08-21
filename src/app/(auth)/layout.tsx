d018cd9a8c8bd1fc072cecc11e8c9eefe48253d4
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-08-21 09:34:50 -0300

    fix: adjust max-width of AuthLayout for better responsiveness

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex flex-col w-full max-w-100 m-auto space-y-2">
+    <div className="flex flex-col w-full max-w-[90%] sm:max-w-100 m-auto space-y-2">
       <Link href="/" className="cursor-default">
         <Logo />
       </Link>
