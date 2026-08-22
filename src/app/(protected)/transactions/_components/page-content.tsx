d1716f7c4598eb21b472b41395977bc97a8403a5
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-08-22 10:42:33 -0300

    feat: refactor Transactions page to use PageContent component for improved structure and maintainability

---
+++ src/app/(protected)/transactions/_components/page-content.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import ListSection from "@/app/(protected)/transactions/_components/list-section";
+import PageFilters from "@/app/(protected)/transactions/_components/page-filters";
+import PageHeader from "@/app/(protected)/transactions/_components/page-header";
+import SearchForm from "@/app/(protected)/transactions/_components/search-form";
+import TotalsSection from "@/app/(protected)/transactions/_components/totals-section";
+import { getTransactions } from "@/services/transaction";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { Type } from "../../../../../prisma/generated/enums";
+
+const PageContent = () => {
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState<"ALL" | Type>("ALL");
+
+  const { data: transactionData, isLoading } = useQuery({
+    queryKey: ["transactions", search, filter],
+    queryFn: async () => {
+      const params = { search, filter };
+      return await getTransactions(params);
+    },
+  });
+
+  return (
+    <main className="w-full max-w-5xl flex flex-col space-y-4">
+      <PageHeader />
+      <SearchForm onSearch={(value) => setSearch(value)} />
+
+      <div className="flex items-center justify-between">
+        <PageFilters
+          currentFilter={filter}
+          onFilterChange={(value) => setFilter(value)}
+        />
+        <span className="text-xs text-gray-400">{`${transactionData?.transactions?.length || 0} items`}</span>
+      </div>
+
+      <TotalsSection
+        totalIn={transactionData?.summary?.totalIn || 0}
+        totalOut={transactionData?.summary?.totalOut || 0}
+        filter={filter}
+      />
+
+      <ListSection transactions={transactionData?.transactions || []} />
+    </main>
+  );
+};
+
+export default PageContent;
--- src/app/(protected)/transactions/page.tsx
+++ src/app/(protected)/transactions/page.tsx
@@ -1,49 +1,12 @@
-"use client";
+import PageContent from "@/app/(protected)/transactions/_components/page-content";
+import { Metadata } from "next";
 
-import ListSection from "@/app/(protected)/transactions/_components/list-section";
-import PageFilters from "@/app/(protected)/transactions/_components/page-filters";
-import PageHeader from "@/app/(protected)/transactions/_components/page-header";
-import SearchForm from "@/app/(protected)/transactions/_components/search-form";
-import TotalsSection from "@/app/(protected)/transactions/_components/totals-section";
-import { getTransactions } from "@/services/transaction";
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { Type } from "../../../../prisma/generated/enums";
+export const metadata: Metadata = {
+  title: "Transactions",
+};
 
 const Transactions = () => {
-  const [search, setSearch] = useState("");
-  const [filter, setFilter] = useState<"ALL" | Type>("ALL");
-
-  const { data: transactionData } = useQuery({
-    queryKey: ["transactions", search, filter],
-    queryFn: async () => {
-      const params = { search, filter };
-      return await getTransactions(params);
-    },
-  });
-
-  return (
-    <main className="w-full max-w-5xl flex flex-col space-y-4">
-      <PageHeader />
-      <SearchForm onSearch={(value) => setSearch(value)} />
-
-      <div className="flex items-center justify-between">
-        <PageFilters
-          currentFilter={filter}
-          onFilterChange={(value) => setFilter(value)}
-        />
-        <span className="text-xs text-gray-400">{`${transactionData?.transactions?.length} items`}</span>
-      </div>
-
-      <TotalsSection
-        totalIn={transactionData?.summary?.totalIn || 0}
-        totalOut={transactionData?.summary?.totalOut || 0}
-        filter={filter}
-      />
-
-      <ListSection transactions={transactionData?.transactions || []} />
-    </main>
-  );
+  return <PageContent />;
 };
 
 export default Transactions;
